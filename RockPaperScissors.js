671895f516a38bd5669845bb380e7c183613e985
Brandon Lee <135774657+Brmilee@users.noreply.github.com>
2025-11-13 18:13:39 -0800

    feat: add interactive ui

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,39 @@
 // console.log("Hello World");
 
+ // add buttons to DOM
+ const rockButton = document.querySelector("#rock");
+ const paperButton = document.querySelector("#paper");
+ const scissorsButton = document.querySelector("#scissors");
+
+ // add event listener to play a round of rps with player choices
+ rockButton.addEventListener("click", () => {
+    const humanSelection = "rock";
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+ });
+
+ paperButton.addEventListener("click", () => {
+    const humanSelection = "paper";
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+ });
+ scissorsButton.addEventListener("click", () => {
+    const humanSelection = "scissors";
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+ });
+
+
+
+ // add a div to display results
+ const score = document.querySelector("#score");
+ let playerScore = document.querySelector("#playerScore");
+ let computerScore = document.querySelector("#computerScore");
+
+
+ let totalPlayerScore = 0, totalComputerScore = 0;
+
+
 function getComputerChoice(choice){
    let n = Math.floor(Math.random() * 3);
    
@@ -16,73 +50,70 @@ function getComputerChoice(choice){
    }
    return choice;
 }
-// console.log(getComputerChoice()); Checks to make sure getComputerChoice works
-
-function getHumanChoice(choice){
-    let rpsChoice = prompt("Rock, Paper, or Scissors?");
-
-    //conditionals to determine choice
-    if(rpsChoice.toLowerCase() === "rock"){
-        choice = "Rock";
-    }
-    else if(rpsChoice.toLowerCase() === "paper"){
-        choice = "Paper";
-    }
-    else if(rpsChoice.toLowerCase() === "scissors"){
-        choice = "Scissors";
-    }
-    else{
-        console.log("Invalid Input");
-    }
-
-    return choice;
-}
-
-// console.log(getHumanChoice()); Checks to make sure getHumanChoice works
-
-
 
 // A function that plays a round
 function playRound(humanChoice, computerChoice){
     //player and computer choose the same thing
-    if(humanChoice.toLowerCase() === computerChoice){
+    if(humanChoice === computerChoice){
         console.log("It's a Draw!");
     }
     //human wins
-    if((humanChoice.toLowerCase() === "rock" && computerChoice === "Scissors") || (humanChoice.toLowerCase() === "paper" && computerChoice === "Rock") || (humanChoice.toLowerCase() === "scissors" && computerChoice === "Paper")) {
+    if((humanChoice === "rock" && computerChoice === "Scissors") || (humanChoice === "paper" && computerChoice === "Rock") || (humanChoice === "scissors" && computerChoice === "Paper")) {
         console.log("Round Winner: Player");
-        humanScore++;
+        addPlayerScore();
     }
+    //computer wins
     else{
         console.log("Round Winner: Computer")
-        computerScore++;
+        addComputerScore();
     }
+    didIWin();
    
 
 }
 
-let humanScore = 0, computerScore = 0;
+ // function to add to score to player
+ function addPlayerScore(){
+    totalPlayerScore++
+    playerScore.innerHTML = totalPlayerScore;
+ }
 
-// playRound(humanSelection, computerSelection); //plays a round of Rock, Paper Scissors
+ function addComputerScore(){
+    totalComputerScore++
+    computerScore.innerHTML = totalComputerScore;
+ }
 
-function playGame(){
-    for(i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
 
-        playRound(humanSelection, computerSelection);
+// function determine if winner condition is met
+function didIWin(){
+    if(totalPlayerScore === 5){
+        const playerWin = document.querySelector("#playerWin")
+        playerWin.innerHTML = 'Player Wins the Game! Congrats!!!';
+        
+        //disbale buttons
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        
     }
-
-    if(humanScore > computerScore){
-        console.log("Player Wins the Game! :)");
+    else if(totalComputerScore === 5){
+        console.log("Computer Wins");
+        const cpuWin = document.querySelector("#cpuWin");
+        cpuWin.innerHTML = 'Computer Wins the Game! Bummer :(';
+        
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+    
     }
-    else if(computerScore < humanScore){
-        console.log("Computer Wins the Game...how sad :(");
-    }
-    else{
-        console.log("It's a draw....bummer :/")
-    }
+    
 
 }
 
-playGame();
+
+
+
+
+
+
+
